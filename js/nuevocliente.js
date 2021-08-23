e42538bd1afb6b8f1279014cce94510c1f3da3e7
Fenner Eduardo González Castellanos <fennereduardo@gmail.com>
2021-08-22 20:39:46 -0500

    Se Inicia Control de versiones

---
+++ js/nuevocliente.js
@@ -0,0 +1,30 @@
+import { mostrarAlerta, validar } from './funciones.js'
+import { nuevoCliente } from './API.js'
+
+//Proteger las variables de est archivo con un EEFI
+(function () {
+
+    const formulario = document.querySelector('#formulario')
+    formulario.addEventListener('submit', validarCliente)
+
+    function validarCliente(e) {
+        e.preventDefault()
+
+        const nombre = document.querySelector('#nombre').value
+        const email = document.querySelector('#email').value
+        const telefono = document.querySelector('#telefono').value
+        const empresa = document.querySelector('#empresa').value
+
+        const cliente = { nombre, email, telefono, empresa }
+        
+        if(validar(cliente)){
+            mostrarAlerta('Todos los campos son obligatorios')
+            return
+        }
+        nuevoCliente(cliente)
+    }
+
+    function validar(obj) {
+        return !Object.values(obj).every(input => input != '')
+    }
+})()
